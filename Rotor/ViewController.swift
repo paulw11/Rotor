b26df10dbcd3a87820de8791e8319aa9c7ce25b1
Paul Wilkinson <paulw@wilko.me>
2018-10-12 21:40:47 +1100

    Initial Commit

---
+++ Rotor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rotor
+//
+//  Created by Paul Wilkinson on 12/10/18.
+//  Copyright © 2018 Paul Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
